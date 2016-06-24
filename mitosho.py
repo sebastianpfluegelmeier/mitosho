72698778c6679cf888b2b46a992ba75cc7ca23a3
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2016-06-24 15:55:28 +0200

    added configuration via config file. not tested yet

--- mitosho.py
+++ mitosho.py
@@ -1,45 +1,59 @@
 #!/bin/env python3
 """midi to shortcut utility"""
 
+import sys
 from pykeyboard import PyKeyboard
 import mido
 
-def process_message(in_msg):
+def read_conf():
+    """reads the config file and returns all substitutions and the device"""
+    device = ''
+    midi_to_shortcut = {}
+    try:
+        with open('config') as config:
+            for line in config:
+                if len(line) < 1 or line[0] == '#':
+                    pass
+                elif len(line) > 5 and line[0:7] == 'device:':
+                    device = line[9:]
+                else:
+                    midi, shortcut = line.split(':')
+                    midi = line.split(' ')
+                    shortcut = line.split(' ')
+                    message = mido.Message('note_on', note=midi[0], channel=midi[1])
+                    midi_to_shortcut[message] = shortcut
+
+        if device is "":
+            print("no device specified")
+            sys.exit()
+
+        return (device, midi_to_shortcut)
+    except TypeError:
+        print('Error reading config')
+
+def process_message(in_msg, midi_to_shortcut):
     """ processes message """
     keyboard = PyKeyboard()
     print(in_msg)
     try:
-        """ this are the available key commands:
-        a s d f h g z x c v b q w e r y t 1 2 3 4 6 5 = 9 7 - 8 0 ] o u [ i p l j \
-                ' k ; \\ , / n m . ` \r \t \n return  tab  space  delete  escape
-                command  shift  capslock  option  alternate  control rightshift
-                rightoption  rightcontrol  function  """
-
-        # presses command shift v on note 60 on channel 0
-        if in_msg.note is 60 and in_msg.channel is 0 and in_msg.type is 'note_on':
-
-            keyboard.press_key('command')
-            keyboard.press_key('shift')
-            keyboard.press_key('v')
-            keyboard.release_key('command')
-            keyboard.release_key('shift')
-            keyboard.release_key('v')
-
-        # presses tap on note 59, channel 0 
-        if in_msg.note is 59 and in_msg.channel is 0 and in_msg.type is 'note_on':
-
-            keyboard.tap_key('tab')
+        if mido.Note(in_msg.type, note=in_msg.note, channel=in_msg.channel) in midi_to_shortcut:
+            shortcut = midi_to_shortcut(mido.Note.type, note=in_msg.note, channel=in_msg.channel)
+            keyboard.press_key(shortcut)
+            keyboard.release_key(shortcut)
 
     except AttributeError:
         print('note not recognized')
 
 def main():
-    """main function"""
+    """reads the config file first, then opens the midi input and waits for commands"""
+    device, midi_to_shortcut = read_conf()
+
     print(mido.get_input_names())
     # input the name off your Midi device here.
-    with mido.open_input('IAC-Treiber IAC Bus 1') as input:
-        for message in input:
-            process_message(message)
+    with mido.open_input(device) as inp:
+
+        for message in inp:
+            process_message(message, midi_to_shortcut)
 
 
 if __name__ == '__main__':
